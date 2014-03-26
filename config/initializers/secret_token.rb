55042c8f091534aaca3360af2f35093c77ffecfe
Dragon <dragon@dragon-VirtualBox.(none)>
2014-03-26 20:27:20 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails3DeviseRspecCucumber::Application.config.secret_key_base = '5fb7c7c3f0ff55dcb963f3f09e0e642d81a51bb6e842f89f62d68ff3264ba3e9f3f2e611d6a1c0108c4129aecf8479adcd651d4973e8aefd8e1ec54de3fab7b7'
